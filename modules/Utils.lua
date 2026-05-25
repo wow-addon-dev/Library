61a9d80420be2017dab4da731ab565396f139b8a
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-05-26 00:58:19 +0200

    v1.9

--- modules/Utils.lua
+++ modules/Utils.lua
@@ -1,23 +1,15 @@
 local _, LIB = ...
 
-local L = LIB.Localization
-
 ------------------------
 --- Public Functions ---
 ------------------------
 
---- Returns the unique character key for the current player.
+--- Returns the unique character-realm key for the current player.
 ---
---- @return string charKey The character key in the format "CharacterName#RealmName".
-function ArcaneWizardLibrary.Utils:GetCharKey()
-    local characterName, realmName
-
-    if UnitFullName then
-        characterName, realmName = UnitFullName("player")
-    end
-
-    characterName = characterName or UnitName("player") or "Unknown"
-    realmName = realmName or GetRealmName() or "UnknownRealm"
+--- @return string characterRealmKey The character-realm key in the format "CharacterName#RealmName".
+function ArcaneWizardLibrary.Utils:GetCharacterRealmKey()
+    local characterName = UnitName("player")
+    local realmName = GetRealmName()
 
     return characterName .. "#" .. realmName
 end
